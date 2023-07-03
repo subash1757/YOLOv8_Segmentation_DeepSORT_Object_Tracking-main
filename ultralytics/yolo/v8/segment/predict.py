07017f1f0fa528c859afaba2a40434cf691c0e10
subash1757 <119210050+subash1757@users.noreply.github.com>
2023-07-03 11:44:12 +0530

    Update predict.py

--- ultralytics/yolo/v8/segment/predict.py
+++ ultralytics/yolo/v8/segment/predict.py
@@ -1,7 +1,6 @@
-# Ultralytics YOLO 🚀, GPL-3.0 license
-
 import hydra
 import torch
+from google.colab.patches import cv2_imshow
 
 from ultralytics.yolo.utils import DEFAULT_CONFIG, ROOT, ops
 from ultralytics.yolo.utils.checks import check_imgsz
@@ -10,14 +9,12 @@ from ultralytics.yolo.utils.plotting import colors, save_one_box
 from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 from numpy import random
 
-
 import cv2
 from deep_sort_pytorch.utils.parser import get_config
 from deep_sort_pytorch.deep_sort import DeepSort
-#Deque is basically a double ended queue in python, we prefer deque over list when we need to perform insertion or pop up operations
-#at the same time
 from collections import deque
 import numpy as np
+
 palette = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
 data_deque = {}
 
@@ -33,9 +30,8 @@ def init_tracker():
                             nms_max_overlap=cfg_deep.DEEPSORT.NMS_MAX_OVERLAP, max_iou_distance=cfg_deep.DEEPSORT.MAX_IOU_DISTANCE,
                             max_age=cfg_deep.DEEPSORT.MAX_AGE, n_init=cfg_deep.DEEPSORT.N_INIT, nn_budget=cfg_deep.DEEPSORT.NN_BUDGET,
                             use_cuda=True)
-##########################################################################################
+
 def xyxy_to_xywh(*xyxy):
-    """" Calculates the relative bounding box from absolute pixel values. """
     bbox_left = min([xyxy[0].item(), xyxy[2].item()])
     bbox_top = min([xyxy[1].item(), xyxy[3].item()])
     bbox_w = abs(xyxy[0].item() - xyxy[2].item())
@@ -59,13 +55,10 @@ def xyxy_to_tlwh(bbox_xyxy):
     return tlwh_bboxs
 
 def compute_color_for_labels(label):
-    """
-    Simple function that adds fixed color depending on the class
-    """
-    if label == 0: #person
-        color = (85,45,255)
-    elif label == 2: # Car
-        color = (222,82,175)
+    if label == 0:  # person
+        color = (85, 45, 255)
+    elif label == 2:  # Car
+        color = (222, 82, 175)
     elif label == 3:  # Motobike
         color = (0, 204, 255)
     elif label == 5:  # Bus
@@ -74,169 +67,104 @@ def compute_color_for_labels(label):
         color = [int((p * (label ** 2 - label + 1)) % 255) for p in palette]
     return tuple(color)
 
-
-
 def UI_box(x, img, color=None, label=None, line_thickness=None):
-    # Plots one bounding box on image img
-    tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
-    color = color or [random.randint(0, 255) for _ in range(3)]
+    tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
     cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
     if label:
-        tf = max(tl - 1, 1)  # font thickness
+        tf = max(tl - 1, 1)
         t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+        cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)
+        cv2.putText(
+            img, label, (c1[0], c1[1] - 2),
+            0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
 
-        img =  cv2.rectangle(img, (c1[0], c1[1] - t_size[1] -3), (c1[0] + t_size[0], c1[1]+3), color,-1,  cv2.LINE_AA)
-
-        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
-
-
-
-def draw_boxes(img, bbox, names,object_id, identities=None, offset=(0, 0)):
-    #cv2.line(img, line[0], line[1], (46,162,112), 3)
-
-    height, width, _ = img.shape
-    # remove tracked point from buffer if object is lost
-    for key in list(data_deque):
-      if key not in identities:
-        data_deque.pop(key)
-
-    for i, box in enumerate(bbox):
-        x1, y1, x2, y2 = [int(i) for i in box]
-        x1 += offset[0]
-        x2 += offset[0]
-        y1 += offset[1]
-        y2 += offset[1]
-
-        # code to find center of bottom edge
-        center = (int((x2+x1)/ 2), int((y2+y2)/2))
-
-        # get ID of object
-        id = int(identities[i]) if identities is not None else 0
-
-        # create new buffer for new object
-        if id not in data_deque:  
-          data_deque[id] = deque(maxlen= 64)
-        color = compute_color_for_labels(object_id[i])
-        obj_name = names[object_id[i]]
-        label = '{}{:d}'.format("", id) + ":"+ '%s' % (obj_name)
-
-        # add center to buffer
-        data_deque[id].appendleft(center)
-        UI_box(box, img, label=label, color=color, line_thickness=2)
-        # draw trail
-        for i in range(1, len(data_deque[id])):
-            # check if on buffer value is none
-            if data_deque[id][i - 1] is None or data_deque[id][i] is None:
-                continue
-            # generate dynamic thickness of trails
-            thickness = int(np.sqrt(64 / float(i + i)) * 1.5)
-            # draw trails
-            cv2.line(img, data_deque[id][i - 1], data_deque[id][i], color, thickness)
-    return img
-
-
-class SegmentationPredictor(DetectionPredictor):
-
-    def postprocess(self, preds, img, orig_img):
-        masks = []
-        # TODO: filter by classes
-        p = ops.non_max_suppression(preds[0],
-                                    self.args.conf,
-                                    self.args.iou,
-                                    agnostic=self.args.agnostic_nms,
-                                    max_det=self.args.max_det,
-                                    nm=32)
-        proto = preds[1][-1]
-        for i, pred in enumerate(p):
-            shape = orig_img[i].shape if self.webcam else orig_img.shape
-            if not len(pred):
-                continue
-            if self.args.retina_masks:
-                pred[:, :4] = ops.scale_boxes(img.shape[2:], pred[:, :4], shape).round()
-                masks.append(ops.process_mask_native(proto[i], pred[:, 6:], pred[:, :4], shape[:2]))  # HWC
-            else:
-                masks.append(ops.process_mask(proto[i], pred[:, 6:], pred[:, :4], img.shape[2:], upsample=True))  # HWC
-                pred[:, :4] = ops.scale_boxes(img.shape[2:], pred[:, :4], shape).round()
-
-        return (p, masks)
-
-    def write_results(self, idx, preds, batch):
-        p, im, im0 = batch
-        log_string = ""
-        if len(im.shape) == 3:
-            im = im[None]  # expand for batch dim
-        self.seen += 1
-        if self.webcam:  # batch_size >= 1
-            log_string += f'{idx}: '
-            frame = self.dataset.count
-        else:
-            frame = getattr(self.dataset, 'frame', 0)
-
-        self.data_path = p
-        self.txt_path = str(self.save_dir / 'labels' / p.stem) + ('' if self.dataset.mode == 'image' else f'_{frame}')
-        log_string += '%gx%g ' % im.shape[2:]  # print string
-        self.annotator = self.get_annotator(im0)
-
-        preds, masks = preds
-        det = preds[idx]
-        if len(det) == 0:
-            return log_string
-        # Segments
-        mask = masks[idx]
-        if self.args.save_txt:
-            segments = [
-                ops.scale_segments(im0.shape if self.args.retina_masks else im.shape[2:], x, im0.shape, normalize=True)
-                for x in reversed(ops.masks2segments(mask))]
-
-        # Print results
-        for c in det[:, 5].unique():
-            n = (det[:, 5] == c).sum()  # detections per class
-            log_string += f"{n} {self.model.names[int(c)]}{'s' * (n > 1)}, "  # add to string
-
-        # Mask plotting
-        self.annotator.masks(
-            mask,
-            colors=[colors(x, True) for x in det[:, 5]],
-            im_gpu=torch.as_tensor(im0, dtype=torch.float16).to(self.device).permute(2, 0, 1).flip(0).contiguous() /
-            255 if self.args.retina_masks else im[idx])
-
-        det = reversed(det[:, :6])
-        self.all_outputs.append([det, mask])
-        xywh_bboxs = []
-        confs = []
-        oids = []
-        outputs = []
-        # Write results
-        for j, (*xyxy, conf, cls) in enumerate(reversed(det[:, :6])):
-            x_c, y_c, bbox_w, bbox_h = xyxy_to_xywh(*xyxy)
-            xywh_obj = [x_c, y_c, bbox_w, bbox_h]
-            xywh_bboxs.append(xywh_obj)
-            confs.append([conf.item()])
-            oids.append(int(cls))
-        xywhs = torch.Tensor(xywh_bboxs)
-        confss = torch.Tensor(confs)
-          
-        outputs = deepsort.update(xywhs, confss, oids, im0)
-        if len(outputs) > 0:
-            bbox_xyxy = outputs[:, :4]
-            identities = outputs[:, -2]
-            object_id = outputs[:, -1]
-            
-            draw_boxes(im0, bbox_xyxy, self.model.names, object_id,identities)
-        return log_string
-
-
-@hydra.main(version_base=None, config_path=str(DEFAULT_CONFIG.parent), config_name=DEFAULT_CONFIG.name)
-def predict(cfg):
+@hydra.main(config_path="configs/yolov4.yaml", strict=False)
+def main(cfg):
     init_tracker()
-    cfg.model = cfg.model or "yolov8n-seg.pt"
-    cfg.imgsz = check_imgsz(cfg.imgsz, min_dim=2)  # check image size
-    cfg.source = cfg.source if cfg.source is not None else ROOT / "assets"
 
-    predictor = SegmentationPredictor(cfg)
-    predictor()
+    source, weights, view_img, save_txt, imgsz = cfg.get("source"), cfg.get("weights"), cfg.get("view_img"), cfg.get("save_txt"), cfg.get("imgsz")
+    webcam = source == "0" or source.startswith("rtsp") or source.startswith("http")
 
+    # Initialize
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    half = device.type != "cpu"  # half precision only supported on CUDA
+
+    # Load model
+    model = DetectionPredictor(
+        cfg.get("yaml"),
+        weights,
+        imgsz,
+        device,
+        conf_thres=cfg.get("conf_thres"),
+        iou_thres=cfg.get("iou_thres"),
+        augment=cfg.get("augment"),
+        half=half,
+    )
+
+    # Set Dataloader
+    dataset = model.set_dataloader(source, imgsz)
+
+    # Run inference
+    if webcam:
+        dataset_size = 0
+    else:
+        dataset_size = len(dataset)
+
+    for img, _, _, path in dataset:
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()
+        img /= 255.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        # Inference
+        pred = model.predict(img, save_img=False)
+
+        # Apply NMS
+        pred = model.non_max_suppression(pred, None, None, agnostic=False)
+
+        # Process detections
+        for i, det in enumerate(pred):
+            gn = torch.tensor(img.shape)[[1, 0, 1, 0]]
+            if det is not None and len(det):
+                det[:, :4] = ops.box_xyxy_to_cxcywh(det[:, :4])
+                det[:, :4] = ops.xywh2xyxy(det[:, :4] / gn)
+
+                det[:, :4] = det[:, :4].clamp(min=0, max=1)
+
+                xyxy = det[:, :4].detach().cpu().numpy()
+
+                confs = det[:, 4].detach().cpu().numpy()
+
+                classes = det[:, 5].detach().cpu().numpy()
+
+                for box, conf, clss in zip(xyxy, confs, classes):
+                    x, y, w, h = box
+
+                    x *= imgsz
+                    y *= imgsz
+                    w *= imgsz
+                    h *= imgsz
+
+                    # Tracking
+                    bbox_xywh = xyxy_to_xywh(x, y, x + w, y + h)
+                    bbox_tlwh = xyxy_to_tlwh([bbox_xywh])
+
+                    frame = cv2.cvtColor(img.permute(0, 2, 3, 1).cpu().numpy()[0], cv2.COLOR_RGB2BGR)
+
+                    features = deepsort.extract_features(frame, bbox_tlwh)
+                    outputs = deepsort.update(bbox_tlwh, features)[0]
+                    if len(outputs) > 0:
+                        bbox_xyxy = outputs[:, :4]
+                        identities = outputs[:, -1]
+                        for i, box in enumerate(bbox_xyxy):
+                            x1, y1, x2, y2 = [int(i) for i in box]
+                            label = f"{int(identities[i])}"
+                            color = compute_color_for_labels(int(identities[i]))
+                            UI_box((x1, y1, x2, y2), frame, color=color, label=label)
+                    cv2_imshow(frame)
 
 if __name__ == "__main__":
-    predict()
+    main()
